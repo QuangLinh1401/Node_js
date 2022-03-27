d9b743532ac5ec0f99b2f0c64c2f067414c748f8
QuangLinh1401 <92560293+QuangLinh1401@users.noreply.github.com>
2022-03-27 16:07:15 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, 'resources/views'));
 //console.log('PATH: ', path.join(__dirname, 'resources/views')) //xem đường dẫn
-
+app.use(express.static(path.join(__dirname, 'public')))
 //
 //HTTP logger
 app.use(morgan('combined'));
